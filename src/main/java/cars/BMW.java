83aa136835ab3afbcc844128e9f8c81502ef66b0
akash <iamakashbhoyar@gmail.com>
2022-12-31 10:59:54 +0530

    update bmw

--- src/main/java/cars/BMW.java
+++ src/main/java/cars/BMW.java
@@ -4,6 +4,7 @@ public class BMW {
 	
 	public void  speed() {
 		System.out.println("Well Done akash you finally Did it");
+		System.out.println("haaa swapnya jamla ka nahi");
 
 	}
 	public void milage() {
