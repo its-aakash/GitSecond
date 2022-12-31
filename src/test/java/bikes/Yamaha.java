ef9a4311d37f2c0000caf12d548631bc201cf684
akash <iamakashbhoyar@gmail.com>
2022-12-31 09:19:58 +0530

    yamaha bike updated

--- src/test/java/bikes/Yamaha.java
+++ src/test/java/bikes/Yamaha.java
@@ -17,10 +17,17 @@ public class Yamaha {
 		}
 		@Test
 		public void milage() {
+			System.out.println("50 km per litre");
+			System.out.println("80 km per litre");
+			System.out.println("90 km per litre");
 
 		}
 		@Test
 		public void zeroTo100() {
+			System.out.println(" in just 4 sec");
+			System.out.println(" in just 5 sec");
+			System.out.println(" in just 10 sec");
+
 
 		}
 
